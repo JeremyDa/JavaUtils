ce9426d653dd1a8d6e3f588f4790fbf9fc9e6854
jeremy da <705653685@qq.com>
2017-09-03 22:18:15 +0800

    union some utils

---
+++ src/main/java/com/jeremy/utils/JavaUtilsApplication.java
@@ -0,0 +1,12 @@
+package com.jeremy.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUtilsApplication.class, args);
+	}
+}
